76f03ac2c3050f1a75eefd24aa2fdd17fbf2c380
Hoemins <shghlals1717@gmail.com>
2024-01-09 15:39:49 +0900

    왜조래 선진

--- Assets/Scripts/Item/QuickSlot.cs
+++ Assets/Scripts/Item/QuickSlot.cs
@@ -43,7 +43,7 @@ public class QuickSlot : MonoBehaviour
             else if (slots[i].item == null)
             {
                 slots[i].SetImage(item);
-
+                slots[i].CountItem++;
                 return;
             }
         }
