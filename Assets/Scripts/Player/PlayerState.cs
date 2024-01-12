85af7fac8baa2b890fb7f01ee54c4664f91da08a
장민정 <152954488+mongmoung@users.noreply.github.com>
2024-01-13 03:59:58 +0900

    플레이어 애니메이션 추가

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -113,12 +113,12 @@ public class CaughtState: PlayerState //몬스터한테 잡혔을때
     {
         Debug.Log("한번만 제발");
         player.playerMove.enabled = false;
-        if (player.aim.isLookMonster && player.oneSlot.Slots[0].items.Count > 0)
+/*        if (player.aim.isLookMonster && player.oneSlot.Slots[0].items.Count > 0)
         {
             player.StartCoroutine(CaughtCo());
         }
         else
-            Debug.Log("주금");
+            Debug.Log("주금");*/
     }
     protected IEnumerator CaughtCo()
     {
