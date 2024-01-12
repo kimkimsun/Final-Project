a5b9fcbf72f4dc500961c666767c71a05dc03e75
MJ <wldhkfpahs@naver.com>
2024-01-12 21:35:30 +0900

    커밋

---
+++ Assets/Scripts/UI/Setting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setting : MonoBehaviour
+{
+    Button closeButton;
+    Button menuButton;
+    Button settingButton;
+    Button exitButton;
+
+    private void Start()
+    {
+        //menuButton.onClick.AddListener(); // 메뉴씬으로 이동
+    }
+
+}
