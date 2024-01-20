23bddcc4647ceb0567fca4fb1fabc4ec6a46c7f0
장민정 <wldhkfpahs@naver.com>
2024-01-20 17:06:02 +0900

    메인메뉴 ui작업중

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -6,15 +6,9 @@ using UnityEngine.UI;
 public class UIManager : SingleTon<UIManager>
 {
     public Image openUI;
-    public AudioClip ClickSound;
     public Image escapeCircle;
     public GameObject settingBox;
     public UseItemInfo useItemInfo;
     public Stack<Object> UIStack = new Stack<Object>();
-
-
-    public void PlayClickSound()
-    {
-        SoundManager.Instance.Play(ClickSound);
-    }
 }
+
---
+++ Assets/Scripts/UI/PopOn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PopUpOn : MonoBehaviour, IPointerClickHandler
+{
+    public AudioClip clip;
+    public GameObject popUp;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SoundManager.Instance.Play(clip);
+        SetOnPopUp(popUp);
+    }
+
+    public void SetOnPopUp(GameObject popUpName)
+    {
+        popUp.SetActive(true);
+    }
+}
