68c58a085108222a1202ff7dfc867639ab8b25c0
kimkimsun <mojung2205@naver.com>
2024-07-17 22:04:58 +0900

    스크립트만

--- Assets/Scripts/etc_/ItemSpawnObject.cs
+++ Assets/Scripts/etc_/ItemSpawnObject.cs
@@ -7,7 +7,6 @@ public class ItemSpawnObject : MonoBehaviour
     {
         int randomSpawn = Random.Range(0, 2);
 
-
         if(randomSpawn == 0)
         {
             Item spawnPrefab = itemSpawnList[Random.Range(0, itemSpawnList.Count)];
